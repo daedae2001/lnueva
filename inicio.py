4400ca58646643c559eab29a3c976873eb12c753
daedae2001 <daedae2001@gmail.com>
2022-03-14 20:53:31 +0100

    s

--- inicio.py
+++ inicio.py
@@ -4,14 +4,14 @@ import m_une_csvs
 import m_crea_csv_todo_limpio
 import m_control_urls
 import m_crea_m3u
+"""
 mylist = [["es.m3u8", 'https://i.mjh.nz/PlutoTV/es.m3u8', 'es'],
           ["mx.m3u8", 'https://i.mjh.nz/PlutoTV/mx.m3u8', 'mx'],
           ["r1.m3u8", 'https://raw.githubusercontent.com/emiliocuesta/milista/7539e0504f56413d853dcc01ccaf978a531e64a9/lista12', '']]
 """
-mylist = [["es.m3u8", 'https://github.com/daedae2001/lista/raw/main/es.m3u8', 'es'],
-          ["mx.m3u8", 'https://github.com/daedae2001/lista/raw/main/mx.m3u8', 'mx'],
-          ["r1.m3u8", 'https://github.com/daedae2001/lista/raw/main/r1.m3u8', '']]
-"""
+mylist = [
+    ["r2.m3u8", 'https://raw.githubusercontent.com/daedae2001/lnueva/master/l1.m3u', '']]
+
 
 for x in mylist:
     m_descarga.descarga(x[0], x[1], x[2])
